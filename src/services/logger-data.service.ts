bc2b41a8cc6bf22a5d5ca7965dc30a808e8d1a72
Joey Wills <jdwills95+github@gmail.com>
2023-01-24 21:06:41 -0600

    Added LoggerDataService.

---
+++ src/services/logger-data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class LoggerDataService {
+  constructor(private http: HttpClient) { }
+  
+  private baseUrl = 'http://localhost:8000'
+  private routes = {
+      getPingData: (fromDate: Date, toDate: Date) => `${this.baseUrl}/ping/${fromDate}/${toDate}`,
+      getSpeedData: (fromDate: Date, toDate: Date) => `${this.baseUrl}/ping/${fromDate}/${toDate}`
+    };
+
+  getPingData(fromDate: Date, toDate: Date) {
+    return this.http.get(this.routes.getPingData(fromDate, toDate))      
+    .subscribe((data: any) => {
+
+    });
+  }
+
+  getSpeedData(fromDate: Date, toDate: Date) {
+    return this.http.get(this.routes.getSpeedData(fromDate, toDate))      
+    .subscribe((data: any) => {
+
+    });
+  }
+}
