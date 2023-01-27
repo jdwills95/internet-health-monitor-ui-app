c114ef0c2b2000eb5c43c253cb59f31028923471
Joey Wills <jdwills95+github@gmail.com>
2023-01-26 20:08:36 -0600

    Finished fixing up services.

--- src/app/features/main-page/main-page.component.ts
+++ src/app/features/main-page/main-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { LoggerDataService } from 'src/services/logger-data/logger-data.service'
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+
+  constructor(
+    private loggerDataService: LoggerDataService
+  ) {
+    const fromDate = new Date('22 January 2023 02:00 UTC');
+    const toDate = new Date('22 January 2023 15:00 UTC');
+
+    const x = this.loggerDataService
+    .getPingData(fromDate, toDate);
+  }
 
 }
--- src/services/logger-data/logger-data.service.ts
+++ src/services/logger-data/logger-data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PingData } from 'src/models/ping/ping-data.class';
+import { SpeedData } from 'src/models/speed/speed-data.class'
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class LoggerDataService {
@@ -7,21 +10,27 @@ export class LoggerDataService {
   
   private baseUrl = 'http://localhost:8000'
   private routes = {
-      getPingData: (fromDate: Date, toDate: Date) => `${this.baseUrl}/ping/${fromDate}/${toDate}`,
-      getSpeedData: (fromDate: Date, toDate: Date) => `${this.baseUrl}/ping/${fromDate}/${toDate}`
+      getPingData: (fromDate: Date, toDate: Date) => `${this.baseUrl}/ping/${fromDate.toISOString()}/${toDate.toISOString()}`,
+      getSpeedData: (fromDate: Date, toDate: Date) => `${this.baseUrl}/ping/${fromDate.toISOString()}/${toDate.toISOString()}`
     };
 
   getPingData(fromDate: Date, toDate: Date) {
     return this.http.get(this.routes.getPingData(fromDate, toDate))      
     .subscribe((data: any) => {
-      debugger;
+      if(data?.length) {
+        return data.map((p: PingData)  => plainToInstance(PingData, p))
+      }
+      return data;
     });
   }
 
   getSpeedData(fromDate: Date, toDate: Date) {
     return this.http.get(this.routes.getSpeedData(fromDate, toDate))      
     .subscribe((data: any) => {
-
+      if(data?.length) {
+        return data.map((p: SpeedData)  => plainToInstance(SpeedData, p))
+      }
+      return data;
     });
   }
 }
