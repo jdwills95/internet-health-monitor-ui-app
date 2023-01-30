b408123beb1862294c13bbba17f9b16e0c42c3c4
Joey Wills <jdwills95+github@gmail.com>
2023-01-29 20:07:38 -0600

    Adjusted setting of envs.

---
+++ src/assets/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // @ts-ignore
+  apiUrl: window["env"]["apiUrl"] || "http://localhost:8000",
+};
+  
--- src/assets/environment/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    
-    production: false,
-    // @ts-ignore
-    backendBaseUrl: window['env']['apiBaseUrl'] || 'http://localhost:8000'
-  };
-  
