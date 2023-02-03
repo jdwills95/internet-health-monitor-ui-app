355044a64145f99ea028f5f884085a439f98e348
Joey Wills <jdwills95+github@gmail.com>
2023-02-02 19:42:50 -0600

    Adjusted gitbump.sh #5

--- bump/gitbump.sh
+++ bump/gitbump.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
-set -e
-
-git fetch --tags # checkout action does not get these
-
-# -v:refname is a version sort
-oldv=$(git tag --sort=-v:refname --list "v[0-9]*" | head -n 1)
-echo "oldv: $oldv"
-
-# if there is no version tag yet, let's start at 0.0.0
-if [ -z "$oldv" ]; then
-   echo "No existing version, starting at 0.0.0"
-   oldv="0.0.0"
-fi
-
-echo inc $oldv
-newv="$(inc $oldv)"
-echo "newv: $newv"
-
-git tag -a "v$newv" -m "version $newv"
-git push --follow-tags
-
 function inc()
 {
     split(s, a, ".")
@@ -48,3 +27,24 @@ function join()
         s = s a[i] "."
     return s a[y]
 }
+
+set -e
+
+git fetch --tags # checkout action does not get these
+
+# -v:refname is a version sort
+oldv=$(git tag --sort=-v:refname --list "v[0-9]*" | head -n 1)
+echo "oldv: $oldv"
+
+# if there is no version tag yet, let's start at 0.0.0
+if [ -z "$oldv" ]; then
+   echo "No existing version, starting at 0.0.0"
+   oldv="0.0.0"
+fi
+
+echo "$(inc $oldv)"
+newv="$(inc $oldv)"
+echo "newv: $newv"
+
+git tag -a "v$newv" -m "version $newv"
+git push --follow-tags
