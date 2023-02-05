b09b9ee7072a44d02d8f0075b2d04c0855b3fb7c
Actions <jdwills95@gmail.com>
2023-02-04 21:11:20 -0600

    Added speed data grid.

--- src/components/speed-data-grid/speed-data-grid.component.ts
+++ src/components/speed-data-grid/speed-data-grid.component.ts
@@ -1,10 +1,129 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { ColumnApi, GridApi, GridReadyEvent } from 'ag-grid-community';
+
+import { LoggerDataService } from 'src/services/logger-data/logger-data.service';
+import { ISpeedData } from 'src/models/speed/speed-data.interface';
 
 @Component({
   selector: 'app-speed-data-grid',
   templateUrl: './speed-data-grid.component.html',
-  styleUrls: ['./speed-data-grid.component.scss']
+  styleUrls: ['./speed-data-grid.component.scss'],
 })
 export class SpeedDataGridComponent {
+  public speedDataLoaded: boolean = false;
 
+  private gridApi!: GridApi<ISpeedData>;
+  private gridColumnApi!: ColumnApi;
+
+  speedDates = new FormGroup({
+    fromDate: new FormControl<Date>(this.getFromDefaultDate()),
+    toDate: new FormControl<Date>(new Date()),
+    filterIpDrop: new FormControl<string>('All'),
+  });
+
+  public speedRowData!: ISpeedData[];
+
+  speedColumnDefs = [
+    {
+      headerName: 'Date Time',
+      field: 'dateTime',
+      sortable: true,
+      resizable: true,
+    },
+    {
+      headerName: 'Download Speed (MBps)',
+      field: 'speedDown',
+      sortable: true,
+      resizable: true,
+    },
+    {
+      headerName: 'Upload Speed (MBps)',
+      field: 'speedUp',
+      sortable: true,
+      resizable: true,
+    },
+  ];
+
+  speedData: ISpeedData[] = [];
+  speedDataFiltered: ISpeedData[] = [];
+
+  fromDate = this.speedDates.controls['fromDate'].value
+    ? this.speedDates.controls['fromDate'].value
+    : this.getFromDefaultDate();
+
+  toDate = this.speedDates.controls['toDate'].value
+    ? this.speedDates.controls['toDate'].value
+    : new Date();
+
+  constructor(private loggerDataService: LoggerDataService) {
+    this.speedDataLoaded = false;
+    this.getSpeedData();
+  }
+
+  getSpeedData() {
+    this.speedDataLoaded = false;
+
+    const fromDate = this.speedDates.controls['fromDate'].value
+      ? this.speedDates.controls['fromDate'].value
+      : new Date(new Date().setMonth(new Date().getMonth() - 1));
+
+    const toDate = this.speedDates.controls['toDate'].value
+      ? this.speedDates.controls['toDate'].value
+      : new Date();
+
+    this.loggerDataService.getSpeedData(fromDate, toDate).then((pd) => {
+      this.speedData = pd;
+      this.onSpeedFilterChanged();
+    });
+  }
+
+  dateChange(type: string) {
+    if (!this.checkDateRange()) {
+      if (type == 'speed') {
+        this.getSpeedData();
+      }
+    }
+  }
+
+  getFromDefaultDate(): Date {
+    return new Date(new Date().setMonth(new Date().getMonth() - 1));
+  }
+
+  checkDateRange() {
+    if (
+      this.speedDates.controls.fromDate.value != null &&
+      this.speedDates.controls.toDate.value
+    ) {
+      return (
+        this.speedDates.controls.fromDate.value >
+        this.speedDates.controls.toDate.value
+      );
+    }
+    return false;
+  }
+
+  onSpeedFilterChanged() {
+    this.speedDataFiltered = this.speedData;
+    this.speedRowData = this.speedDataFiltered;
+    this.speedDataLoaded = true;
+  }
+
+  autoSizeAll(skipHeader: boolean) {
+    const allColumnIds: string[] = [];
+    this.gridColumnApi.getColumns()!.forEach((column) => {
+      allColumnIds.push(column.getId());
+    });
+    this.gridColumnApi.autoSizeColumns(allColumnIds, skipHeader);
+  }
+
+  onGridReady(params: GridReadyEvent<ISpeedData>) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.speedRowData = this.speedDataFiltered;
+
+    this.autoSizeAll(false);
+  }
 }
--- src/services/logger-data/logger-data.service.ts
+++ src/services/logger-data/logger-data.service.ts
@@ -1,64 +1,68 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PingData } from 'src/models/ping/ping-data.class';
-import { SpeedData } from 'src/models/speed/speed-data.class'
+import { SpeedData } from 'src/models/speed/speed-data.class';
 import * as moment from 'moment';
-import { environment } from 'src/assets/environment'
+import { environment } from 'src/assets/environment';
 
 @Injectable()
 export class LoggerDataService {
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
+
   private baseUrl = environment.apiUrl;
   private routes = {
-      getPingData: (fromDate: Date, toDate: Date) => 
-      `${this.baseUrl}/ping/${this.formatDateForAPI(fromDate)}/${this.formatDateForAPI(toDate)}`,
-      getSpeedData: (fromDate: Date, toDate: Date) => 
-      `${this.baseUrl}/ping/${this.formatDateForAPI(fromDate)}/${this.formatDateForAPI(toDate)}`
-    };
+    getPingData: (fromDate: Date, toDate: Date) =>
+      `${this.baseUrl}/ping/${this.formatDateForAPI(
+        fromDate
+      )}/${this.formatDateForAPI(toDate)}`,
+    getSpeedData: (fromDate: Date, toDate: Date) =>
+      `${this.baseUrl}/speedtest/${this.formatDateForAPI(
+        fromDate
+      )}/${this.formatDateForAPI(toDate)}`,
+  };
 
   async getPingData(fromDate: Date, toDate: Date): Promise<Array<PingData>> {
     return new Promise((resolve, reject) => {
-      
-      this.http.get(this.routes.getPingData(fromDate, toDate))
-      .subscribe((response: any) => {
-        if(response){
-          let pingData: PingData[] = [];
-          pingData = response.map((p: any)  => {
-            const pingData: PingData = {
-              dateTime: moment.utc(p.date_time).toDate(),
-              ip: p.ip.trim(),
-              packetsReceived: Number(p.packets_received),
-              packetsSent: Number(p.packets_sent),
-              latency: Number(p.latency),
-            }
-            return pingData;
-          });
-          resolve(pingData);
-        }
-      })
-    })
+      this.http
+        .get(this.routes.getPingData(fromDate, toDate))
+        .subscribe((response: any) => {
+          if (response) {
+            let pingData: PingData[] = [];
+            pingData = response.map((p: any) => {
+              const pingData: PingData = {
+                dateTime: moment.utc(p.date_time).toDate(),
+                ip: p.ip.trim(),
+                packetsReceived: Number(p.packets_received),
+                packetsSent: Number(p.packets_sent),
+                latency: Number(p.latency),
+              };
+              return pingData;
+            });
+            resolve(pingData);
+          }
+        });
+    });
   }
 
   async getSpeedData(fromDate: Date, toDate: Date): Promise<Array<SpeedData>> {
     return new Promise((resolve, reject) => {
-      
-      this.http.get(this.routes.getSpeedData(fromDate, toDate))
-      .subscribe((response: any) => {
-        if(response){
-          let speedData: SpeedData[] = [];
-          speedData = response.map((p: any)  => {
-            const speedData: SpeedData = {
-              dateTime: moment.utc(p.date_time).toDate(),
-              speedDown: p.speed_down,
-              speedUp: p.speed_up,
-            }
-            return speedData;
-          });
-          resolve(speedData);
-        }
-      })
-    })
+      this.http
+        .get(this.routes.getSpeedData(fromDate, toDate))
+        .subscribe((response: any) => {
+          if (response) {
+            let speedData: SpeedData[] = [];
+            speedData = response.map((p: any) => {
+              const speedData: SpeedData = {
+                dateTime: moment.utc(p.date_time).toDate(),
+                speedDown: this.convertBytesToMegabyte(p.speed_down),
+                speedUp: this.convertBytesToMegabyte(p.speed_up),
+              };
+              return speedData;
+            });
+            resolve(speedData);
+          }
+        });
+    });
   }
 
   private convertLocalDateToUTC(date: Date) {
@@ -71,5 +75,14 @@ export class LoggerDataService {
 
   private formatDateForAPI(date: Date) {
     return this.convertDateToISOString(this.convertLocalDateToUTC(date));
+  }
+
+  private convertBytesToMegabyte(byte: number) {
+    if (byte > 0) {
+      const megabyte = byte / 1000000;
+      return Math.trunc(megabyte);
+    }
+
+    return 0;
   }
 }
